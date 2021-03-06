a7a1ecb0b485e863463e0f6fd407782f4d1c6b1b
Jason Fisher <jason@wanax.com>
2021-03-06 13:53:25 -0500

    fixed invalid auth test

--- server/middleware/validateRequest.js
+++ server/middleware/validateRequest.js
@@ -6,14 +6,10 @@ module.exports = (req, res, next) => {
   const key = req.headers.key;
 
   if (token && uname && key) {
-    if (uname.split('@')[1] === 'xavier.edu') {
-      try {
-        auth.validate(req, res, next, token, uname, key);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    } else {
-      res.status(401).send({ message: 'Users must have an active Xavier University email to use this system.' });
+    try {
+      auth.validate(req, res, next, token, uname, key);
+    } catch (err) {
+      res.status(500).send(err);
     }
   } else {
     res.status(401).send({ message: 'Invalid token or key' });
